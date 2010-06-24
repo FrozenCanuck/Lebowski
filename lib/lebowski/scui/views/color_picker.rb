3d41cf1a8b97e6244fc7bd6a165bd16aa5241622
Dessy Daskalov <dessy.daskalov@gmail.com>
2010-06-24 14:43:47 -0400

    adding proxy for SCUI.ColorPicker

---
+++ lib/lebowski/scui/views/color_picker.rb
@@ -0,0 +1,12 @@
+module Lebowski
+  module SCUI
+    module Views
+      
+      class ColorPickerView < Lebowski::Foundation::Views::View
+        representing_sc_class 'SCUI.ColorPicker'
+        
+      end
+      
+    end
+  end
+end
