1144031bff9f279ec290773d7af701250146e7a6
Michael Cohen <michael.lee.cohen@gmail.com>
2010-08-17 16:31:11 -0400

    added a hello world spec file with comments removed

---
+++ examples/hello_world/spec/hello_world_no_comments_spec.rb
@@ -0,0 +1,55 @@
+# ==========================================================================
+# Project:   Lebowski Framework - The SproutCore Test Automation Framework
+# License:   Licensed under MIT license (see License.txt)
+# ==========================================================================
+
+# require '../../../lib/lebowski/spec'
+# 
+# include Lebowski::Foundation
+# include Lebowski::Foundation::Views
+
+App = MainApplication.new \
+        :app_root_path => "/hello_world", 
+        :app_name => "HelloWorldApp",
+        :browser => :firefox 
+
+App.start do |app|
+  app['isLoaded'] == true
+end
+
+App.move_to 1, 1 
+App.resize_to 1024, 768 
+
+App.define 'group', 'mainPage.mainPane.groupView', View
+
+describe "Hello World Test" do
+  
+  before(:all) do    
+    @label = App['group.label', 'SC.LabelView']
+    @hello_button = App['group.helloButton', ButtonView]
+    @world_button = App['#world-button', ButtonView]
+  end
+  
+  it "will check that label has an initial value 'click a button'" do
+    @label.should have_value /click a button/i
+  end
+  
+  it "will check that hello button has title 'hello'" do
+    @hello_button.should have_title /hello/i
+  end
+  
+  it "will check that world button has title 'world'" do
+    @world_button.should have_title /world/i
+  end
+  
+  it "will click hello button and then confirm label has value 'hello'" do
+    @hello_button.click
+    @label.should have_value /hello/i
+  end
+
+  it "will click world button and then confirm label has value 'world'" do
+    @world_button.click
+    @label.should have_value /world/i
+  end
+  
+end
