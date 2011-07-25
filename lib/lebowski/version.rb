d1aaa5c1bef65996fd22443283f4eea283bba13c
Michael Cohen <michael.lee.cohen@gmail.com>
2011-07-25 16:29:31 -0700

    bumped version to 0.4.0

--- lib/lebowski/version.rb
+++ lib/lebowski/version.rb
@@ -6,8 +6,8 @@
 module Lebowski
   module Version
     MAJOR = 0
-    MINOR = 3
-    PATCH = "3.pre.0"
+    MINOR = 4
+    PATCH = 0
     BUILD = nil
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
